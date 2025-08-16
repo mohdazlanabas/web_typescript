a06c4c1b39e579413c7556467b8f0cc0ce80b8ad
Roger Woolie <rogerwoolie@RogerW-MacBook-Air.local>
2025-08-16 22:57:41 +0800

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let username = "Roger Woolie";
+console.log("Hello, " + username + "! Welcome to TypeScript.");
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+let username = "Roger Woolie";
+console.log("Hello, " + username + "! Welcome to TypeScript.");
+
+let a:number = 12;
+let b:number = 6;
+let c:number = 2;
+
+console.log(a / b);
+console.log(c * b)
